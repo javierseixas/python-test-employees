f2a4cb99573126c2a2346880c0a28f5a5f0bd0f8
Javier Seixas <dev@javierseixas.com>
2018-02-21 20:36:10 -0300

    Adapts app fixtures to initial example

--- app.py
+++ app.py
@@ -15,29 +15,36 @@ class App(object):
         print(self._accountant.calculate_staff_salary(staff))
 
     def _create_staff_fixtures(self):
-        return Staff([self._new_employee(10, 'hourly', [10, 5])])
+        return Staff([
+            self._new_employee(150, 'hourly', [5, 5]),
+            self._new_employee(200, 'hourly', [10, 5, 5]),
+            self._new_employee(3000, 'fixed', [40, 40, 40, 38]),
+            self._new_employee(5000, 'fixed', [40, 40, 40, 43]),
+            self._new_employee(0, 'voluntary', [50, 50, 60, 50]),
+        ])
 
 
     def _new_employee(self, salary, contract, tracked_hours):
         if contract == 'hourly':
-            cards = []
-            for hours in tracked_hours:
-                cards.append(Card(hours))
+            cards = self._from_hours_to_cards(tracked_hours)
             return Employee(Hourly(salary), HourlyCalculator(), cards)
 
         if contract == 'voluntary':
-            cards = []
-            for hours in tracked_hours:
-                cards.append(Card(hours))
-            return Employee(Voluntary(), HourlyCalculator(), cards)
+            cards = self._from_hours_to_cards(tracked_hours)
+            return Employee(Voluntary(), VoluntaryCalculator(), cards)
 
         if contract == 'fixed':
-            cards = []
-            for hours in tracked_hours:
-                cards.append(Card(hours))
-            return Employee(Fixed(salary), HourlyCalculator(), cards)
+            cards = self._from_hours_to_cards(tracked_hours)
+            return Employee(Fixed(salary), FixedCalculator(), cards)
 
         pass
+
+    def _from_hours_to_cards(self, tracked_hours):
+        cards = []
+        for hours in tracked_hours:
+            cards.append(Card(hours))
+        return cards
+
 
 app = App()
 app.execute()
